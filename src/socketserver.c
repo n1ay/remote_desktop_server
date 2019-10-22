b8ae7e0ed72ed157457cce51f506abd8cd69147a
kamil <kammmilkos@gmail.com>
2019-10-22 19:38:28 +0200

    use select to detect closed connections and handle multiple clients

--- src/socketserver.c
+++ src/socketserver.c
@@ -1,12 +1,16 @@
 #include "socketserver.h"
 
 void* socket_server_start(void* socksrv_attr) {
-    int s, sfd, new_fd;
+    int s, sfd, new_fd, max_fd;
     struct addrinfo hints, *res, *rp;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
+
+    fd_set master, read_fds;
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds);
 
     s = getaddrinfo(NULL, ((SocketServerAttr*) socksrv_attr)->port, &hints, &res);
     if (s != 0) {
@@ -33,21 +37,61 @@ void* socket_server_start(void* socksrv_attr) {
     }
     freeaddrinfo(res);
 
-    struct sockaddr_storage peer_addr;
+    struct sockaddr_in peer_addr;
     socklen_t peer_addr_len;
     ssize_t nread;
-    char* initbuf = (char*) malloc(BUF_SIZE);
-    memset(initbuf, 0, BUF_SIZE);
+    char* readbuf = (char*) malloc(BUF_SIZE);
+    memset(readbuf, 0, BUF_SIZE);
     
     if (listen(sfd, BACKLOG)) {
-        perror("Could not listen");
+        perror("listen");
     }
-    
-    new_fd = accept(sfd, (struct sockaddr*) &peer_addr, &peer_addr_len);
 
+    FD_SET(sfd, &master);
+    max_fd = sfd;
+    
     while(1) {
-        send(new_fd, *((SocketServerAttr*) socksrv_attr)->buf, *((SocketServerAttr*) socksrv_attr)->bufsize, 0);
+        read_fds = master;
+        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(1);
+        }
+
+        for (int i = 0; i <= max_fd; i++) {
+            if (FD_ISSET(i, &read_fds)) {
+                if (i == sfd) {
+                    // new connection                 
+                    if ((new_fd = accept(sfd, (struct sockaddr*) &peer_addr, &peer_addr_len)) == -1) {
+                        perror("accept");
+                    } else {
+                        FD_SET(new_fd, &master);
+                        if (new_fd > max_fd) {
+                            max_fd = new_fd;
+                        }
+                        printf("New connection from %s on socket %d\n", inet_ntoa(peer_addr.sin_addr), new_fd);
+                    }
+                } else {
+                    // data from already connected client
+                    nread = recv(i, readbuf, sizeof(readbuf), 0);
+                    if (nread <= 0) {
+                        if (nread == -1) {
+                            perror("recv");
+                        } else {
+                            printf("Socket %d hung up\n", i);
+                        }
+                        close(i);
+                        FD_CLR(i, &master);
+                    } else {
+                        send(i, readbuf, nread, 0);
+                    }
+                }
+            }
+        }
     }
+
+    /*while(1) {
+        send(new_fd, *((SocketServerAttr*) socksrv_attr)->buf, *((SocketServerAttr*) socksrv_attr)->bufsize, 0);
+    }*/
 }
 
 void init_socksrv_attr (SocketServerAttr* socksrv_attr, char* port, unsigned char** buf, unsigned long* bufsize) {
